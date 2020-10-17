13d9f59f98e92fa1182711759874937869e39199
Eric Young <eyoung88@frontier.com>
2020-10-17 00:27:36 -0600

    front-end and set up

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Home } from "./Home";
+import { Login } from "./Login";
+import { Account } from "./Account";
+import { Register } from "./Register";
+import { Edit } from "./Edit";
+import Dashboard from "./material-dashboard-react-master/material-dashboard-react-master/src/views/Dashboard/Dashboard";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/account">Account</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          {/* Home */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          {/* Login Page */}
+          <Route path="/login">
+            <Login />
+          </Route>
+          {/* Sign up page */}
+          <Route path="/register">
+            <Register />
+          </Route>
+
+          {/* Account page */}
+          <Route path="/account">
+            <Dashboard />
+          </Route>
+          {/* Edit page */}
+          <Route path="/edit">
+            <Edit />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
