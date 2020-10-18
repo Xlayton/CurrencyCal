84e91a73c583438d07db5f7fc90fe6f9ff994b5d
unknown <zwslocum@yahoo.com>
2020-10-18 10:56:40 -0600

    update

---
+++ front-end/material-dashboard-react-master/build/precache-manifest.80f4f0696fe4701fd04ec80328690bd2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdc324d2cf50ab2330c62032f50d83c",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "5253609324fc370c6420",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "b422dbeb0cd26ad2455e",
+    "url": "/material-dashboard-react/static/css/main.5ee3f738.chunk.css"
+  },
+  {
+    "revision": "5253609324fc370c6420",
+    "url": "/material-dashboard-react/static/js/2.627895d3.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/material-dashboard-react/static/js/2.627895d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b422dbeb0cd26ad2455e",
+    "url": "/material-dashboard-react/static/js/main.118f4de5.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/material-dashboard-react/static/js/main.118f4de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a025ab54ec03fed337bb",
+    "url": "/material-dashboard-react/static/js/runtime-main.d0bf2bb1.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "6aa18605746d671591dc1d554e41d219",
+    "url": "/material-dashboard-react/static/media/ugly.6aa18605.jpg"
+  }
+]);
